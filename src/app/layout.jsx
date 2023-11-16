bc766557043ce854dfd1bc6d2991e0e833b03764
Hannes Samskog <hannessamskog@gmail.com>
2023-11-16 22:00:19 +0100

    Yeah

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,8 +5,8 @@ import '@/styles/tailwind.css'
 
 export const metadata = {
   title: {
-    template: '%s - TaxPal',
-    default: 'TaxPal - Accounting made simple for small businesses',
+    template: '%s - HyrFokus',
+    default: 'HyrFokus - Administration utan krångel, boende utan besvär',
   },
   description:
     'Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited.',
